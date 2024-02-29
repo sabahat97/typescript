315b754bb26ba37962d4a0aabcb201f5f7a94d04
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-02-29 19:44:52 +0500

    Add files via upload

---
+++ var.ts
@@ -0,0 +1,25 @@
+// basic data type 
+
+let Name : string = "sabahat"; //string
+let age : number = 19; //number
+let isStudent : boolean =  true ; //boolean
+let github_id : any = 'https://github.com/sabahat97/const-data.ts'; //any
+
+
+// basic data type (i self made)
+
+let data: {
+
+      person : string
+      age : number     
+      isStudent : boolean
+      github_id : any
+
+} = { 
+         person : "sabahat", 
+         age : 13,
+        isStudent : true,
+        github_id:  'https://github.com/sabahat97/const-data.ts',
+
+}
+console.log (data);
